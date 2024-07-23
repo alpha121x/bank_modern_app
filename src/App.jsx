e13bb274eae30b800248d1670666c61d2149dc44
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-07-24 01:05:19 +0500

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <div>
-      App
+import style from './style';
+
+const App = () =>  (
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className='{`${styles.paddingX} ${styles.flexCenter}`}'>
+        <div className='{`${styles.boxWidth}`}'>
+          Navbar
+        </div>
+      </div>
     </div>
-  )
-}
+  );
 
 export default App
